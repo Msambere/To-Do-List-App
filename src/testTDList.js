9dec13228946071fd0545011e2cfda64dfb39432
Msambere <110812972+Msambere@users.noreply.github.com>
2024-06-13 21:27:00 -0400

    Update test tasks descriptions
    Add the info in the test tasks descriptions that the local storage must
    be cleared in order to see the correct dates.

--- src/testTDList.js
+++ src/testTDList.js
@@ -5,19 +5,19 @@ import { Todo} from "./constructors";
 // Hardcode for testing
 const mainTodoList = [];
 
-hardCodeTDs("Always Quad 1","check view",addDays(new Date(), 1),"High","To-do App",);
-hardCodeTDs("Always Quad 2 bc of priority","check view",addDays(new Date(), 15),"High","To-do App",);
-hardCodeTDs("Always Quad 2 bc of date","check view",addDays(new Date(), 4),"Medium","To-do App",);
-hardCodeTDs("Always Quad 3 bc of date","check view",addDays(new Date(), 6),"Low","To-do App",);
-hardCodeTDs("Always Quad 3 bc of priority","check view",addDays(new Date(), 15),"Medium","To-do App",);
-hardCodeTDs("Go to the gym","Complete the daily workout","","High", "Daily",);
+hardCodeTDs("Always Quad 1","must clear local storage to update",addDays(new Date(), 1),"High","To-do App",);
+hardCodeTDs("Always Quad 2 bc of priority","must clear local storage to update",addDays(new Date(), 15),"High","To-do App",);
+hardCodeTDs("Always Quad 2 bc of date","must clear local storage to update",addDays(new Date(), 4),"Medium","To-do App",);
+hardCodeTDs("Always Quad 3 bc of date","must clear local storage to update",addDays(new Date(), 6),"Low","To-do App",);
+hardCodeTDs("Always Quad 3 bc of priority","must clear local storage to update",addDays(new Date(), 15),"Medium","To-do App",);
+hardCodeTDs("Go to the gym","populate daily list","","High", "Daily",);
 hardCodeTDs("tester1", "checking fn", "04-11-1989", "Low", "Test 1");
 hardCodeTDs("tester2", "checking fn", "05-18-2020", "Medium", "Test 2");
-hardCodeTDs("Always today", "always today", new Date(), "Low", "Test 1");
-hardCodeTDs("Today plus 2","checking fn", addDays(new Date(), 2),"High","Test 2",);
-hardCodeTDs("Today plus 5","checking fn", addDays(new Date(), 5),"Medium","Test 3",);
+hardCodeTDs("Always today", "must clear local storage to update", new Date(), "Low", "Test 1");
+hardCodeTDs("Today plus 2","must clear local storage to update", addDays(new Date(), 2),"High","Test 2",);
+hardCodeTDs("Today plus 5","must clear local storage to update", addDays(new Date(), 5),"Medium","Test 3",);
 hardCodeTDs("read the newspaper","populate daily list","","Medium","Daily",);
-hardCodeTDs("Always Quad 4","should be there",addDays(new Date(), 20),"Low","Test 1",);
+hardCodeTDs("Always Quad 4","must clear local storage to update",addDays(new Date(), 20),"Low","Test 1",);
 // Functions
 function indexList(tdList) {
   tdList.forEach((tdObject) => {
